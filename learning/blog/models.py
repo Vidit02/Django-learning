9a1c70016b78debd84a455e84c9ffb32f37da9dc
“Vidit <vidit.21102@gmail.com>
2022-11-08 22:38:28 +0530

    Using of Models done

--- learning/blog/models.py
+++ learning/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    #do not write id
+    name = models.CharField(max_length = 100)
+    email = models.EmailField(max_length = 100)
+    age = models.IntegerField()
+    isActive = models.BooleanField(default = True)
+    password = models.CharField(max_length = 100)
+    address = models.TextField()
+    salary = models.FloatField(null=True)
+    mobile = models.BigIntegerField(null=True)
+    createdat = models.DateTimeField(auto_now_add=True,null=True)
+
+
+
+    #class meta to give meta information
+    class Meta :
+        db_table : "employee"
+
+class Job(models.Model):
+    title : models.CharField(max_length=100)
+    
